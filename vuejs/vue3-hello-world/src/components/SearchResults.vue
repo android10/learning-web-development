9910fb444b14fe5021e28fb0ab10c2494ca85ff2
Fernando Cejas <fcejas@gmail.com>
2021-05-02 19:12:18 +0200

    Finish example.

---
+++ vuejs/vue3-hello-world/src/components/SearchResults.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="root">
+    <p> Showing {{ filteredTitles.length }} results for "{{ query }}" </p>
+    <ul>
+      <li
+        v-for="title in filteredTitles"
+        :key="title.Page"
+      >
+        {{ title.Name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { computed, onMounted } from 'vue'
+import titles from '../post-data.json'
+
+export default {
+  props: {
+    query: {
+      type: String,
+      default: ''
+    }
+  },
+
+  setup (props) {
+    const filteredTitles = computed(() => {
+      return titles.filter(title => title.Name.toLowerCase().includes(props.query.toLowerCase()))
+    })
+      
+    onMounted(() => {
+      console.log('mounted')
+    })
+    
+    return {
+      filteredTitles
+    }
+  }
+}
+</script>
+
+<style scoped>
+	.root {
+		width: 400px;
+		margin: 0 auto;
+	}
+	.root p {
+		text-align: right;
+		font-size: 0.7em;
+		margin: 0;
+	}
+	ul {
+		list-style:  none;
+		width: 50px 0;
+		padding: 0;
+		background-color: #fafafa;
+		border-radius: 5px;
+		border: 1px solid #ddd;
+	}
+	li {
+		text-align: left;
+		padding: 20px;
+		border-bottom: 1px solid #ddd;
+	}
+	li:nth-last-of-type(1) {
+		border-bottom: none;
+	}
+</style>
